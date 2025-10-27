e9b8b10aa3aeb6ffe6eb758851c8b0803f514103
ananyadobhal <ananyadobhal2006@gmail.com>
2025-10-27 03:40:35 +0000

    Memory Alloca on Techniques: First fit

---
+++ first_fit.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+int main()
+{
+    int p,m;
+    printf("Enter number of free blocks available : ");
+    scanf("%d",&m);
+    int *memory = (int*)malloc(m*sizeof(int));
+    printf("Enter SIZE OF each free blocks : ");
+    for(int i=0;i<m;i++){
+        scanf("%d",&memory[i]);
+    }
+
+    printf("Enter number of processes : ");
+    scanf("%d",&p);
+    int *process = (int*)malloc(p*sizeof(int));
+    printf("Enter size of processes : ");
+    for(int i=0;i<p;i++){
+        scanf("%d",&process[i]);
+    }
+    bool *allocated = (bool*)malloc(m*sizeof(bool));
+    for(int i=0;i<m;i++) allocated[i] = false;
+    int *result = (int*)malloc(p*sizeof(int));
+    for(int i=0;i<m;i++) result[i] = -1;
+
+    for(int i=0;i<p;i++)
+    {
+        int pro = process[i];
+        for(int j=0;j<m;j++){
+            if(memory[j]>pro && allocated[j] == false){
+                allocated[j] = true;
+                result[i] = j+1;
+                break;
+            }
+        }
+    }
+    for(int i=0;i<p;i++){
+        printf("%d : %d\n",process[i], result[i]);
+    }
+
+}
